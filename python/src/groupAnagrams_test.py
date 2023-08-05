864979e843f3f41c3c04c455c112b31a209e9ddf
Alberto Murillo <albertomurillosilva@gmail.com>
2023-08-05 12:10:27 -0700

    Add namedtuple to groupAnagrams_test.py
    This is an example on how to use namedtuples to increase
    readability of code.
    
    When the values of the tuple are long, is easy to loose track to
    what field they map to.
    
    With namedtuples, we can refer to the fields by name using the dot
    notation and we no longer need to remember what index maps to
    what field, increasing readability and maintainability.

--- python/src/groupAnagrams_test.py
+++ python/src/groupAnagrams_test.py
@@ -1,35 +1,38 @@
+from collections import namedtuple
 from typing import List
 
 import pytest
 
 from groupAnagrams import Solution
 
+Test = namedtuple("Test", "given want")
+
 
 @pytest.mark.parametrize(
-    "given, want",
+    "test",
     (
-        (
-            ["eat", "tea", "tan", "ate", "nat", "bat"],
-            [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]],
+        Test(
+            given=["eat", "tea", "tan", "ate", "nat", "bat"],
+            want=[["bat"], ["nat", "tan"], ["ate", "eat", "tea"]],
         ),
-        (
-            [""],
-            [[""]],
+        Test(
+            given=[""],
+            want=[[""]],
         ),
-        (
-            ["a"],
-            [["a"]],
+        Test(
+            given=["a"],
+            want=[["a"]],
         ),
     ),
 )
-def test_groupAnagrams(given: List[str], want: List[List[str]]):
+def test_groupAnagrams(test: Test):
     solution = Solution()
 
-    got = solution.groupAnagrams(given)
+    got = solution.groupAnagrams(test.given)
     sorted_got = [sorted(i) for i in got]
     sorted_got = sorted(sorted_got)
 
-    sorted_want = [sorted(i) for i in want]
+    sorted_want = [sorted(i) for i in test.want]
     sorted_want = sorted(sorted_want)
 
     assert sorted_got == sorted_want
