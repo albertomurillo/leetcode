a677ba54b032450b0496233b76843794f783fec6
Alberto Murillo <albertomurillosilva@gmail.com>
2023-10-30 15:14:08 -0700

    https://leetcode.com/problems/invert-binary-tree

---
+++ go/invertTree.go
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/invert-binary-tree
+
+package leetcode
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
