19b53cc018d177d726b08a7750a0253fbb33276a
Alberto Murillo <albertomurillosilva@gmail.com>
2023-11-03 10:00:00 -0700

    https://leetcode.com/problems/task-scheduler

---
+++ python/src/leastInterval.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/task-scheduler
+
+import heapq
+from collections import Counter, deque
+from typing import List
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        max_heap = [(-count, task) for task, count in Counter(tasks).items()]
+        heapq.heapify(max_heap)
+
+        q = deque()
+
+        ticks = 0
+        while max_heap or q:
+            ticks += 1
+
+            if q:
+                # Send task to the heap if task is ready
+                ready_at_ticks, count, task = q[0]
+                if ready_at_ticks == ticks:
+                    q.popleft()
+                    heapq.heappush(max_heap, (count, task))
+
+            if max_heap:
+                # Process one of the most frequent tasks
+                count, task = heapq.heappop(max_heap)
+                count += 1
+
+                # Send remaining tasks to the queue
+                if count:
+                    q.append((ticks + n + 1, count, task))
+
+        return ticks
