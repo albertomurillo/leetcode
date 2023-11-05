76299d6a1b535e18ee3382f0b3559e43e5a7a66d
Alberto Murillo <albertomurillosilva@gmail.com>
2023-11-05 13:59:12 -0800

    https://leetcode.com/problems/kth-smallest-element-in-a-bst

---
+++ python/src/kthSmallest.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst
+
+from leetcode import TreeNode
+
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        node = root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            k -= 1
+            if not k:
+                return node.val
+
+            node = node.right
+
+        raise ValueError
