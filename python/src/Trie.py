1cbc516270643bf0f49a10f1431510c927ab541e
Alberto Murillo <albertomurillosilva@gmail.com>
2023-11-04 23:51:24 -0700

    https://leetcode.com/problems/implement-trie-prefix-tree

---
+++ python/src/Trie.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/implement-trie-prefix-tree
+
+
+class Trie:
+    def __init__(self):
+        self._root = {}
+
+    def insert(self, word: str) -> None:
+        node = self._root
+        for c in word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node["EOW"] = True
+
+    def search(self, word: str) -> bool:
+        node = self._root
+        for c in word:
+            if c not in node:
+                return False
+            node = node[c]
+        return "EOW" in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self._root
+        for c in prefix:
+            if c not in node:
+                return False
+            node = node[c]
+        return True
