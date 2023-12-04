eecf742a9660e542d342fdb8445a7ca3c1880ff8
Alberto Murillo <albertomurillosilva@gmail.com>
2023-12-04 09:22:17 -0800

    https://leetcode.com/problems/time-based-key-value-store

---
+++ python/src/TimeMap.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/time-based-key-value-store
+
+from collections import defaultdict
+
+
+class TimeMap:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.cache = {}
+        self.versions = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.cache[(key, timestamp)] = value
+        self.versions[key].append(timestamp)
+
+    def get(self, key: str, timestamp: int) -> str:
+        versions = self.versions[key]
+        candidate_timestamp = None
+
+        left = 0
+        right = len(versions) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if versions[mid] <= timestamp:
+                candidate_timestamp = versions[mid]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return self.cache.get((key, candidate_timestamp), "")
