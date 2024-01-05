a82f16640eba34250ed724b2f9f073fc69d503ff
Alberto Murillo <albertomurillosilva@gmail.com>
2024-01-05 14:21:35 -0800

    https://leetcode.com/problems/minimum-interval-to-include-each-query

---
+++ python/src/problem_1851_minInterval.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/minimum-interval-to-include-each-query
+
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        ranges = [range(x[0], x[1] + 1) for x in sorted(intervals)]
+        results = {}
+
+        min_heap = []
+        range_idx = 0
+        for query in sorted(queries):
+            while range_idx < len(ranges) and ranges[range_idx][0] <= query:
+                r = ranges[range_idx]
+                heappush(min_heap, (len(r), r.stop))
+                range_idx += 1
+
+            while min_heap and min_heap[0][1] <= query:
+                heappop(min_heap)
+
+            results[query] = min_heap[0][0] if min_heap else -1
+
+        return [results[q] for q in queries]
---
+++ python/src/problem_1851_minInterval_test.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import pytest
+
+from problem_1851_minInterval import Solution
+
+solution = Solution()
+
+
+@pytest.mark.parametrize(
+    "intervals, queries, want",
+    (
+        (
+            [[1, 4], [2, 4], [3, 6], [4, 4]],
+            [2, 3, 4, 5],
+            [3, 3, 1, 4],
+        ),
+        (
+            [[2, 3], [2, 5], [1, 8], [20, 25]],
+            [2, 19, 5, 22],
+            [2, -1, 4, 6],
+        ),
+    ),
+)
+def test_minInterval(intervals: List[List[int]], queries: List[int], want: List[int]):
+    got = solution.minInterval(intervals, queries)
+    assert got == want
