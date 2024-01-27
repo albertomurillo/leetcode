f783d4a059cfccfd19e26bd0202b88b8c2531328
Alberto Murillo <albertomurillosilva@gmail.com>
2024-01-27 13:26:41 -0800

    https://leetcode.com/problems/median-of-two-sorted-arrays

---
+++ python/src/problem_4_findMedianSortedArrays.py
@@ -0,0 +1,73 @@
+# https://leetcode.com/problems/median-of-two-sorted-arrays
+
+import math
+from typing import Generator, List
+
+
+class Solution:
+    def findMedianSortedArrays(self, a: List[int], b: List[int]) -> float:
+        if len(a) > len(b):
+            return self.findMedianSortedArrays(a=b, b=a)
+
+        size = len(a) + len(b)
+        is_even = size % 2 == 0
+        mid = size // 2
+
+        left, right = 0, len(a) - 1
+        while True:
+            i = (left + right) // 2
+            j = mid - i - 2
+
+            a_left = a[i] if i >= 0 else -math.inf
+            a_right = a[i + 1] if (i + 1) < len(a) else math.inf
+            b_left = b[j] if j >= 0 else -math.inf
+            b_right = b[j + 1] if (j + 1) < len(b) else math.inf
+
+            # partition is correct
+            if a_left <= b_right and b_left <= a_right:
+                if is_even:
+                    return (max(a_left, b_left) + min(a_right, b_right)) / 2
+                return min(a_right, b_right)
+
+            if a_left > b_right:
+                right = i - 1
+            else:
+                left = i + 1
+
+    def findMedianSortedArrays_ptrs(self, nums1: List[int], nums2: List[int]) -> float:
+        def merge(a, b) -> Generator[int, None, None]:
+            pa, pb = 0, 0
+            while pa < len(a) and pb < len(b):
+                if a[pa] <= b[pb]:
+                    yield a[pa]
+                    pa += 1
+                else:
+                    yield b[pb]
+                    pb += 1
+            while pa < len(a):
+                yield a[pa]
+                pa += 1
+            while pb < len(b):
+                yield b[pb]
+                pb += 1
+
+        size = len(nums1) + len(nums2)
+        is_even = size % 2 == 0
+        m = size // 2
+
+        tmp = merge(nums1, nums2)
+        stop = m - (1 if is_even else 0)
+        for _ in range(stop):
+            next(tmp)
+
+        if is_even:
+            return (next(tmp) + next(tmp)) / 2
+
+        return float(next(tmp))
+
+    def findMedianSortedArrays_naive(self, nums1: List[int], nums2: List[int]) -> float:
+        tmp = sorted(nums1 + nums2)
+        m = len(tmp) // 2
+        if len(tmp) % 2 == 0:
+            return (tmp[m - 1] + tmp[m]) / 2
+        return float(tmp[m])
---
+++ python/src/problem_4_findMedianSortedArrays_test.py
@@ -0,0 +1,25 @@
+import pytest
+
+from problem_4_findMedianSortedArrays import Solution
+
+solution = Solution()
+
+
+@pytest.mark.parametrize(
+    "fn",
+    (
+        (solution.findMedianSortedArrays),
+        (solution.findMedianSortedArrays_ptrs),
+        (solution.findMedianSortedArrays_naive),
+    ),
+)
+@pytest.mark.parametrize(
+    "nums1, nums2, want",
+    (
+        ([1, 3], [2], 2.0),
+        ([1, 2], [3, 4], 2.5),
+    ),
+)
+def test_findMedianSortedArrays(fn, nums1, nums2, want):
+    got = fn(nums1, nums2)
+    assert got == want
