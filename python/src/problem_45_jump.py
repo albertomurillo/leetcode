fe5ec3f3d8306102ebedd0cfb8186637e1e2037d
Alberto Murillo <albertomurillosilva@gmail.com>
2024-02-06 17:16:03 -0800

    https://leetcode.com/problems/jump-game-ii

---
+++ python/src/problem_45_jump.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/jump-game-ii
+
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        goal = len(nums) - 1
+        left = 0
+        right = 0
+        jumps = 0
+
+        while right < goal:
+            far = 0
+            for i in range(left, right + 1):
+                far = max(far, i + nums[i])
+
+            left = right + 1
+            right = far
+            jumps += 1
+
+        return jumps
---
+++ python/src/problem_45_jump_test.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import pytest
+
+from problem_45_jump import Solution
+
+solution = Solution()
+
+
+@pytest.mark.parametrize(
+    "nums, want",
+    (
+        ([2, 3, 1, 1, 4], 2),
+        ([2, 3, 0, 1, 4], 2),
+    ),
+)
+def test_jump(nums: List[int], want: int):
+    got = solution.jump(nums)
+    assert got == want
