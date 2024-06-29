4c9cae739c921073526b995b93a694c82885aacd
Alberto Murillo <albertomurillosilva@gmail.com>
2024-06-28 20:51:17 -0700

    ruff: C413
    Unnecessary `reversed` call around `sorted()`

--- python/src/problem_853_carFleet.py
+++ python/src/problem_853_carFleet.py
@@ -6,8 +6,8 @@ from typing import List
 class Solution:
     def carFleet(self, target: int, positions: List[int], speeds: List[int]) -> int:
         fleets = []
-        for position, speed in reversed(sorted(zip(positions, speeds, strict=True))):
-            time_to_destination = (target - position) / speed
+        for pos, speed in sorted(zip(positions, speeds, strict=True), reverse=True):
+            time_to_destination = (target - pos) / speed
             if not fleets or time_to_destination > fleets[-1]:
                 fleets.append(time_to_destination)
         return len(fleets)
